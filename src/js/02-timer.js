76ba8227059a96d9cf24edc96998f960796cdfd0
Evgenija-P <e.a.poduzova@gmail.com>
2022-08-14 22:53:01 +0300

    task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -13,6 +13,7 @@ const timeSeconds = document.querySelector('span[data-seconds]');
 
 // const flatpickr = require('flatpickr');
 // const timing = new flatpickr(input, options);
+let selectionDate;
 
 const options = {
   enableTime: true,
@@ -21,8 +22,8 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     selectionDate = selectedDates[0];
-    currentDate = Date.now();
-    if (selectionDate < currentDate) {
+    // currentDate = Date.now();
+    if (selectionDate < Date.now()) {
       Notiflix.Notify.failure('Please choose a date in the future', {
         position: 'center-top',
       });
